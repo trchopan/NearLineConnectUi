de2decd465b33625409dbb47174bd297a86e8b16
Quang Tran (Chop Tr) <logan1011001@gmail.com>
2022-07-21 20:53:15 +0700

    Highlight token if it is current wallet is owner

--- src/presentation/pages/NFT/NftGrid.svelte
+++ src/presentation/pages/NFT/NftGrid.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import {nearProfile} from '@/application/useNearAuth'
+
   import {Link} from 'svelte-navigator'
 
   interface NftGridView {
@@ -10,19 +12,37 @@
   }
   export let tokens: NftGridView[] = []
   export let withOwner: boolean = false
+  const isMyToken = (ownerId: string) =>
+    ownerId === $nearProfile.value?.accountId.getOrElse(null)
 </script>
 
 <ul class="grid grid-cols-2 md:grid-cols-3 gap-3">
   {#each tokens as token}
-    <li>
+    <li
+      class={`h-64 flex flex-col border-2 border-dashed ${
+        withOwner && isMyToken(token.ownerId) ? 'border-primary' : ''
+      }`}
+    >
       <Link to={'/nft/' + token.tokenId}>
         <img src={token.img} alt="0" />
       </Link>
-      <p class="font-medium">{token.title}</p>
-      <p>{token.description}</p>
-      {#if withOwner}
-        <p class="text-sm">{token.ownerId}</p>
-      {/if}
+      <div class="px-2 flex flex-col place-content-between h-full">
+        <div>
+          <p class="font-medium">{token.title}</p>
+          <p>{token.description}</p>
+        </div>
+        {#if withOwner}
+          <div
+            class={`truncate text-sm px-1 ${
+              isMyToken(token.ownerId)
+                ? 'bg-primary text-medium'
+                : 'bg-gray-200'
+            }`}
+          >
+            {token.ownerId}
+          </div>
+        {/if}
+      </div>
     </li>
   {/each}
 </ul>
